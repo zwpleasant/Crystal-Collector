b1ccead6deb57e3c15f3351e3487499ef42e4957
Zachary Pleasant <zpleasant10@gmail.com>
2017-12-08 21:51:27 -0600

    pseudocoding for the js functionality and game rules

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// variables needed: wins, losses, game value, user total score, crystal 1, crystal 2, crystal 3, crystal 4
+
+// process for game loop
+// randomly generate value of jewel 1, jewel 2, jewel 3, jewel 4 values (number between 1-12)
+// randomly generate game value (number between 19-120)
+// on click function for each jewel that adds its number to game value
+// game is over when user total score equals or is larger than game value
+// if user score is less than game value, continue
+// if user score is equal to game value, log win for user and restart
+// if user score is more than game value, log loss for user and restart
