556246df746ca1d16e1de4591f39ef83445d4c52
Pleasant <zpleasant@anl.gov>
2017-12-12 08:14:39 -0600

    added js functionality for all jewel buttons to populate user score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   // process for game loop
     var winCount = 0; // counter for game wins
     var lossCount = 0; // counter for game losses
-    var userScore = 0;
-    var gameScore = Math.floor(Math.random() * 102) + 19; // creates random number between 19-120
-    var crystalOne = Math.floor(Math.random() * 12) + 1; // creates random number between 1-12
-    var crystalTwo = Math.floor(Math.random() * 12) + 1; // creates random number between 1-12
-    var crystalThree = Math.floor(Math.random() * 12) + 1; // creates random number between 1-12
-    var crystalFour = Math.floor(Math.random() * 12) + 1; // creates random number between 1-12
+    var userScore = 0; // counter for user score
+    var gameScore = Math.floor(Math.random() * 102) + 19; // creates game score, a random number between 19-120
+    var crystalOne = Math.floor(Math.random() * 12) + 1; // creates crystal 1 value, a random number between 1-12
+    var crystalTwo = Math.floor(Math.random() * 12) + 1; // creates crystal 2 value, a random number between 1-12
+    var crystalThree = Math.floor(Math.random() * 12) + 1; // creates crystal 3 value, a random number between 1-12
+    var crystalFour = Math.floor(Math.random() * 12) + 1; // creates crystal 4 value, a random number between 1-12
     $("#gScore").append(" " + gameScore);
     $("#uScore").append(" " + userScore);
     console.log(gameScore);
@@ -30,12 +30,52 @@ $(document).ready(function() {
   // create function for a game win
   // create function for a game loss
 
-  // create on click function for crystalOne
-  // create on click function for crystalTwo
-  // create on click function for crystalThree
-  // create on click function for crystalFour
-  $(".btn").click(function() {
+  // creates on click function for crystalOne
+  $("#crystal1").click(function() {
+    userScore = userScore + crystalOne; // adds value of crystalOne to userScore
+    console.log("User Score: " + userScore);
+    $("#uScore").text("User Score: " + userScore);
+      // if (userScore === gameScore) {
+      //   // function for a game win
+      // } else if {
+      //   //function for a game loss
+      // }
+  });
 
+  // creates on click function for crystalTwo
+  $("#crystal2").click(function() {
+    userScore = userScore + crystalTwo; // adds value of crystalTwo to userScore
+    console.log("User Score: " + userScore);
+    $("#uScore").text("User Score: " + userScore);
+      // if (userScore === gameScore) {
+      //   // function for a game win
+      // } else if {
+      //   //function for a game loss
+      // }
+  });
+
+  // creates on click function for crystalThree
+  $("#crystal3").click(function() {
+    userScore = userScore + crystalThree; // adds value of crystalThree to userScore
+    console.log("User Score: " + userScore);
+    $("#uScore").text("User Score: " + userScore);
+      // if (userScore === gameScore) {
+      //   // function for a game win
+      // } else if {
+      //   //function for a game loss
+      // }
+  });
+
+  // creates on click function for crystalFour
+  $("#crystal4").click(function() {
+    userScore = userScore + crystalFour; // adds value of crystalFour to userScore
+    console.log("User Score: " + userScore);
+    $("#uScore").text("User Score: " + userScore);
+      // if (userScore === gameScore) {
+      //   // function for a game win
+      // } else if {
+      //   //function for a game loss
+      // }
   });
 
   // if user score is less than game value, continue
