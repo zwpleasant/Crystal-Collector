d53649c80680f614105f4bce2bfb23517ea6ca83
Zachary Pleasant <zpleasant10@gmail.com>
2017-12-11 21:49:57 -0600

    added startGame js functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,44 @@
 $(document).ready(function() {
   // process for game loop
 
-  // set variables for win count, loss count, game score, user score, and crystals one, two, three and four
+  // set variables for tracking win count and loss count
   var winCount = 0;
   var lossCount = 0;
 
-  // create a function that sets game score to random number (between 19-120) and sets random values to crystals 1/2/3/4
-  function reset() {
-    var gameScore = Math.floor(Math.random() * 102) + 19; // creates random number between 19-120
+  // resets the values of the game back to 0
+  function resetGame() {
+    var gameScore = 0;
     var userScore = 0;
+    var crystalOne = 0;
+    var crystalTwo = 0;
+    var crystalThree = 0;
+    var crystalFour = 0;
+    $("#uScore").append(" " + userScore);
+  }
+
+  // sets game score to random number (between 19-120) and sets random values to crystals 1/2/3/4
+  function startGame() {
+    var userScore = 0;
+    var gameScore = Math.floor(Math.random() * 102) + 19; // creates random number between 19-120
     var crystalOne = Math.floor(Math.random() * 12) + 1; // creates random number between 1-12
     var crystalTwo = Math.floor(Math.random() * 12) + 1; // creates random number between 1-12
     var crystalThree = Math.floor(Math.random() * 12) + 1; // creates random number between 1-12
     var crystalFour = Math.floor(Math.random() * 12) + 1; // creates random number between 1-12
+    $("#gScore").append(" " + gameScore);
+    $("#uScore").append(" " + userScore);
     console.log(gameScore);
-    console.log(userScore);
     console.log(crystalOne);
     console.log(crystalTwo);
     console.log(crystalThree);
     console.log(crystalFour);
   }
 
-  reset();
+  startGame();
 
   $(".btn").click(function() {
-    alert("You clicked this button");
+    
   });
 
-  // on click function for each jewel that adds its number to user score
-  // create a function for game play that involves if statement
   // if user score is less than game value, continue
   // if user score is equal to game value, log win for user and restart
   // if user score is more than game value, log loss for user and restart
